39b5b4df739f5559b9601d76c66c042896a26bc6
DCGrost <alamjgl_2@hotmail.com>
2022-02-09 19:15:43 -0600

    Controles en pantalla
    Implementación funcional de controles en pantalla, también se elimino la función de doble salto debido a bugs, se pensará en buscar una solución en la etapa de solución de bugs.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [Header("General")]
     public float gravityScale = -20f;
     [Header("Movement")]
+    public FixedJoystick joystick;
     public float walkSpeed = 5f;
     public float dashDistance = 5f;
     public float dashCD = 5f;
@@ -23,9 +24,10 @@ public class Player : MonoBehaviour
     Vector3 moveInput = Vector3.zero;
     Vector3 rotateInput = Vector3.zero;
     CharacterController characterController;
-    bool canMove = true;
     bool isDashing = false;
     float gravityScaleTem = 0f;
+    bool jumpButton = false;
+    bool canMove = true;
 
     private void Awake()
     {
@@ -33,29 +35,23 @@ public class Player : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Move();
-        Look();
-        if (Input.GetButton("Fire3") && !isDashing)
+        if (canMove)
         {
-            StartCoroutine(Dash());
+            Move();
+            Look();
         }
     }
+    #region Move&Look
     private void Move()
     {
         if (characterController.isGrounded)
         {
-            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveInput = new Vector3(joystick.Direction.x, 0f, joystick.Direction.y);
             moveInput = Vector3.ClampMagnitude(moveInput, 1f);
             moveInput = transform.TransformDirection(moveInput * walkSpeed);
-            if (Input.GetButtonDown("Jump"))
+            if (jumpButton)
             {
-                playerAnimator.SetTrigger("Jump");
-                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
-            }
-            if (Input.GetButtonDown("Fire1"))
-            {
-                playerAnimator.SetTrigger("Ability");
+                Jump();
             }
         }
         moveInput.y += gravityScale * Time.deltaTime;
@@ -64,9 +60,11 @@ public class Player : MonoBehaviour
     }
     private void Look()
     {
-        rotateInput.x = Input.GetAxis("Horizontal") * rotationSensivility * Time.deltaTime;
+        rotateInput.x = joystick.Direction.x * rotationSensivility * Time.deltaTime;
         transform.Rotate(Vector3.up * rotateInput.x);
     }
+    #endregion
+    #region Dash
     IEnumerator Dash()
     {
         StartCoroutine(InDash());
@@ -78,13 +76,53 @@ public class Player : MonoBehaviour
     {
         transform.transform.GetChild(0).gameObject.SetActive(false);
         shadowSystem.Play();
-        canMove = false;
         gravityScaleTem = gravityScale;
         gravityScale = 0f;
         yield return new WaitForSeconds(0.1f);
         characterController.Move(this.gameObject.transform.forward * dashDistance);
         transform.transform.GetChild(0).gameObject.SetActive(true);
-        canMove = true;
         gravityScale = gravityScaleTem;
     }
+    public void DashButton()
+    {
+        if (!isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+    #endregion
+    #region Jump
+    private void Jump()
+    {
+        playerAnimator.SetTrigger("Jump");
+        moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
+    }
+    public void JumpButton()
+    {
+        StartCoroutine(WaitJumpButton());
+    }
+    IEnumerator WaitJumpButton()
+    {
+        jumpButton = true;
+        yield return new WaitForSeconds(0.05f);
+        jumpButton = false;
+    }
+    #endregion
+    #region Ability
+    public void Ability()
+    {
+        if (characterController.isGrounded)
+        {
+            StartCoroutine(WaitAbility());
+        }
+    }
+    IEnumerator WaitAbility()
+    {
+        canMove = false;
+        playerAnimator.SetTrigger("Ability");
+        yield return new WaitForSeconds(1f);
+        Debug.Log(canMove);
+        canMove = true;
+    }
+    #endregion
 }
